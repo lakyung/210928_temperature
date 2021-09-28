3678f5f56672937b8aa0ffaadc04f843f4bd71d0
lakyung <90668130+lakyung@users.noreply.github.com>
2021-09-28 12:37:14 +0900

    31도 이상이면 선풍기 켜고 경고음 발생하기

--- main.ts
+++ main.ts
@@ -1,21 +1,25 @@
+music.setVolume(20)
 basic.forever(function () {
     basic.showNumber(input.temperature())
 })
 basic.forever(function () {
-    if (29 <= input.temperature()) {
+    if (31 <= input.temperature()) {
         pins.digitalWritePin(DigitalPin.P2, 1)
         pins.digitalWritePin(DigitalPin.P5, 0)
         pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
         music.ringTone(523)
         basic.pause(100)
     } else if (27 <= input.temperature()) {
         pins.digitalWritePin(DigitalPin.P2, 0)
         pins.digitalWritePin(DigitalPin.P5, 0)
         pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P15, 0)
         music.stopAllSounds()
     } else {
         pins.digitalWritePin(DigitalPin.P2, 1)
         pins.digitalWritePin(DigitalPin.P5, 1)
         pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
     }
 })
